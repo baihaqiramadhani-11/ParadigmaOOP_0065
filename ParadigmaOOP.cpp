ac47db1b742920dbb2499d405bda15fe9d50a5cc
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-04-21 15:19:17 +0700

    data11

--- ParadigmaOOP.cpp
+++ ParadigmaOOP.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 
 class mahasiswa{
-
     public:
     int nim;
     string nama;
     float nilai;
     void printData(){
-        cout<<"NIM = " nim <<endl;
-        cout<<"Nama = " nama <<endl;
-        cout<<"Nilai = "<< nilai <<endl
+        cout<<"NIM = "<< nim <<endl;
+        cout<<"Nama = " << nama <<endl;
+        cout<<"Nilai = "<< nilai <<endl;
     }   
 };
-
 int main(){
     mahasiswa mhs1; 
 
    mhs1.nim = 111;
    mhs1.nama = "Joko Susilo";
    mhs1.nilai = 75.5;
-   mhs1.printData()
+   mhs1.printData();
 }
  
